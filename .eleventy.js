67adc69b4baa0212f5d3fccbb70f2b37b8b28846
Tom Keays <tomkeays@gmail.com>
2022-05-10 18:31:18 -0400

    update nunjucks template configs

--- .eleventy.js
+++ .eleventy.js
@@ -1,6 +1,9 @@
 module.exports = function(eleventyConfig) {
   // Return your Object options:
   return {
+    markdownTemplateEngine: 'njk',
+    dataTemplateEngine: 'njk',
+    htmlTemplateEngine: 'njk',
     dir: {
       input: 'src',
       output: "docs"
