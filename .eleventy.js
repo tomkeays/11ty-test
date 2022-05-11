04a1dc1b5aa2192cd61648808843c53a4d461ee0
Tom Keays <tomkeays@gmail.com>
2022-05-10 21:32:40 -0400

    set up works landing and item pages

--- .eleventy.js
+++ .eleventy.js
@@ -1,8 +1,22 @@
+const sortByDisplayOrder = require('./src/utils/sort-by-display-order.js');
+
 module.exports = config => {
 
-  // Set directories to pass through to the dist folder
+  // Set directories to pass through to the docs folder
   config.addPassthroughCopy('./src/css/');
   config.addPassthroughCopy('./src/images/');
+
+  // Returns work items, sorted by display order
+  config.addCollection('work', collection => {
+    return sortByDisplayOrder(collection.getFilteredByGlob('./src/work/*.md'));
+  });
+
+  // Returns work items, sorted by display order then filtered by featured
+  config.addCollection('featuredWork', collection => {
+    return sortByDisplayOrder(collection.getFilteredByGlob('./src/work/*.md')).filter(
+      x => x.data.featured
+    );
+  });
 
   // Return your Object options:
   return {
